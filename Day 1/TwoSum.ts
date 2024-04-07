3c9d1ec414c8df8b89bdc6262cfa39f2b154bdd5
mrcoder2033d <mrcoder2033d@gmail.com>
2024-04-07 10:03:58 +0000

    Added solution in TypeScript

---
+++ Day 1/TwoSum.ts
@@ -0,0 +1,17 @@
+function twoSum(nums: number[], target: number): number[] {
+    const seen: Map<number, number> = new Map();
+
+    for (let i = 0; ; i++){
+        const x = nums[i];
+        const y = target - x;
+
+        if (seen.has(y)){
+            return [seen.get(y)!, i];
+        }
+
+        seen.set(x, i);
+    }
+};
+
+// Runtime: 57 ms
+// Memory usage: 53.16 MB
