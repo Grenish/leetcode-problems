61d73100c324709b53cbb66293118a4cd50a189e
mrcoder2033d <mrcoder2033d@gmail.com>
2024-04-07 08:35:45 +0000

    Added the solution for the problem

---
+++ Day 1/TwoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const seen = new Map();
+    for (let i = 0; ;i++){
+        const x = nums[i];
+        const y = target - x;
+        if(seen.has(y)){
+            return [seen.get(y), i]
+        }
+        seen.set(x, i);
+    }
+};
