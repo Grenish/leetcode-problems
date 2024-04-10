b538dc324ea440cdb0c75a0d028b177391c310de
mrcoder2033d <mrcoder2033d@gmail.com>
2024-04-10 13:27:30 +0000

    Added solution

---
+++ Day 4/Median.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+
+    let finNum = nums1.concat(nums2).sort((a, b) => a - b);
+    let median = 0;
+    let start = 0;
+    let end = finNum.length - 1;
+    let mid = Math.floor((start + end) / 2);
+
+    if (finNum.length % 2 === 0) {
+      median = (finNum[mid] + finNum[mid + 1]) / 2;
+    } else {
+      median = finNum[mid];
+    }
+
+    return median;
+};
