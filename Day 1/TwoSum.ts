937edc951fcc04263eb068ddafadd6fdba925ec0
mrcoder2033d <mrcoder2033d@gmail.com>
2024-04-11 10:03:38 +0000

    Refractor the code

--- Day 1/TwoSum.ts
+++ Day 1/TwoSum.ts
@@ -1,17 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
-    const seen: Map<number, number> = new Map();
+  const seen: Map<number, number> = new Map();
 
-    for (let i = 0; ; i++){
-        const x = nums[i];
-        const y = target - x;
+  for (let i = 0; ; i++) {
+    const x = nums[i];
+    const y = target - x;
 
-        if (seen.has(y)){
-            return [seen.get(y)!, i];
-        }
-
-        seen.set(x, i);
+    if (seen.has(y)) {
+      return [seen.get(y)!, i];
     }
-};
+
+    seen.set(x, i);
+  }
+}
 
 // Runtime: 57 ms
 // Memory usage: 53.16 MB
